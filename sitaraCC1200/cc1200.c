38e6912cbf11e2b3c6a4bbc11015b250246dfed8
pranavsk1994 <pranavsk1994@gmail.com>
2017-06-13 14:48:33 -0600

    cc1200 init

--- sitaraCC1200/cc1200.c
+++ sitaraCC1200/cc1200.c
@@ -20,6 +20,7 @@
 #include "app_error.h"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
+#include "nrf_delay.h" // Remove later
 
 /* Defines and register set */
 #include "cc1200.h"
@@ -35,7 +36,7 @@
 static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);
  
 static uint8_t buf[1024];
-//static uint8_t tx_buf[1024];
+static uint8_t tx_buf[1024];
 static uint8_t rx_buf[1024];
 static volatile bool spi_xfer_done;
 
@@ -44,11 +45,11 @@ void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
 {
     spi_xfer_done = true;
     NRF_LOG_INFO("Transfer completed.\r\n");
-    if (rx_buf[0] != 0)
-    {
-        NRF_LOG_INFO(" Received: \r\n");
-        NRF_LOG_HEXDUMP_INFO(rx_buf, strlen((const char *)rx_buf));
-    }
+    //if (rx_buf[0] != 0)
+    //{
+    //    NRF_LOG_INFO(" Received: \r\n");
+    //    NRF_LOG_HEXDUMP_INFO(rx_buf, strlen((const char *)rx_buf));
+    //}
 }
 
 int cc1200_cmd_strobe(uint8_t cmd)
@@ -68,12 +69,15 @@ int cc1200_cmd_strobe(uint8_t cmd)
 	return ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);*/
 
 	static uint8_t len = 0;
+	//static uint8_t rx_len = 0;
+	memset(rx_buf, 0, sizeof(rx_buf));
 
-	buf[len++] = cmd;
+	tx_buf[len++] = cmd;
+	//rx_len = len+1;
 
-	NRF_LOG_HEXDUMP_INFO(buf, strlen((const char *)buf));
+	NRF_LOG_HEXDUMP_INFO(tx_buf, strlen((const char *)tx_buf));
 	spi_xfer_done = false;
-	uint8_t status = nrf_drv_spi_transfer(&spi, buf, len, buf, len);
+	uint8_t status = nrf_drv_spi_transfer(&spi, tx_buf, len, rx_buf, len);
 	 
 	APP_ERROR_CHECK(status); // SPI TRANSFER
 	while(!spi_xfer_done){} // wait
@@ -97,10 +101,10 @@ cc1200_get_status(uint8_t *status)
 
 	static uint8_t len = 0;
 
-	buf[len++] = CC1200_SNOP;
+	tx_buf[len++] = CC1200_SNOP;
 
 	spi_xfer_done = false;
-	uint8_t ret = nrf_drv_spi_transfer(&spi, buf, len, buf, len);
+	uint8_t ret = nrf_drv_spi_transfer(&spi, tx_buf, len, rx_buf, len);
 	APP_ERROR_CHECK(ret);
 	while(!spi_xfer_done){} // wait
 	NRF_LOG_FLUSH();
@@ -128,23 +132,26 @@ int cc1200_write_register(uint16_t reg, uint8_t value)
 
 	// Reg
 	static uint8_t len = 0;
+	//static uint8_t rx_len = 0;
 	
+	memset(rx_buf, 0, sizeof(rx_buf));
+
 	if (!CC1200_IS_EXTENDED_ADDR(reg)) {
-		buf[len++] = CC1200_WRITE_BIT | reg;
-		buf[len++] = value;
+		tx_buf[len++] = CC1200_WRITE_BIT | reg;
+		tx_buf[len++] = value;
 	} 
 	// Extended Address
 	else {
-		buf[len++] = CC1200_WRITE_BIT | CC1200_EXT_REG_MASK;
-		buf[len++] = CC1200_UNEXTEND_ADDR(reg);
-		buf[len++] = value;
+		tx_buf[len++] = CC1200_WRITE_BIT | CC1200_EXT_REG_MASK;
+		tx_buf[len++] = CC1200_UNEXTEND_ADDR(reg);
+		tx_buf[len++] = value;
 	}
 
 	// send the SPI message (all of the above fields, inc. buffers)
 	//ret = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
 
 	spi_xfer_done = false;
-	uint8_t ret = nrf_drv_spi_transfer(&spi, buf, len, buf, len);
+	uint8_t ret = nrf_drv_spi_transfer(&spi, tx_buf, len, rx_buf, len);
 	APP_ERROR_CHECK(ret);
 	while(!spi_xfer_done){} // wait
 	NRF_LOG_FLUSH();
@@ -177,44 +184,47 @@ int cc1200_read_register(uint16_t reg, uint8_t *data)
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};*/
-	static uint8_t len = 0;
-
-	memset(rx_buf, 0, len);
+	uint8_t len = 0;
+	uint8_t rx_len = 0;
+	memset(tx_buf, 0, sizeof(tx_buf));
+	memset(rx_buf, 0, sizeof(rx_buf));
 
 	// Reg
 	if (!CC1200_IS_EXTENDED_ADDR(reg)) {
-		buf[len++] = CC1200_READ_BIT | reg;
+		tx_buf[len++] = CC1200_READ_BIT | reg;
 	} 
 	// Extended Address
 	else {
-		buf[len++] = CC1200_READ_BIT | CC1200_EXT_REG_MASK;
-		buf[len++] = CC1200_UNEXTEND_ADDR(reg);
+		tx_buf[len++] = CC1200_READ_BIT | CC1200_EXT_REG_MASK;
+		tx_buf[len++] = CC1200_UNEXTEND_ADDR(reg);
 	}
 
-	buf[len++] = CC1200_SNOP;
+	tx_buf[len++] = CC1200_SNOP;
+	
+	//rx_len = len+1;
 
 	//ret = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
 	//while(!spi_xfer_done){}
 	spi_xfer_done = false;
 	
-	NRF_LOG_INFO("Transfering: ");
-	NRF_LOG_HEXDUMP_INFO(buf, len);
+	NRF_LOG_INFO("Transfering: %x\r\n", len);
+	NRF_LOG_HEXDUMP_INFO(tx_buf, len);
 
-	uint8_t ret = nrf_drv_spi_transfer(&spi, buf, len, rx_buf, len);
+	uint8_t ret = nrf_drv_spi_transfer(&spi, tx_buf, len, rx_buf, rx_len);
 	APP_ERROR_CHECK(ret);
+
 	while(!spi_xfer_done){} // wait
 	
-	NRF_LOG_INFO("Received: ");
-	NRF_LOG_HEXDUMP_INFO(rx_buf, len);
-
+	NRF_LOG_INFO("Received: %x\r\n", rx_len);
+	NRF_LOG_HEXDUMP_INFO(rx_buf, rx_len);
+	//NRF_LOG_INFO("data in buffer is %x , %x , %x , %x\r\n", rx_buf[rx_len-1], rx_buf[rx_len-2], rx_buf[rx_len-3], rx_buf[rx_len-4]);
 	NRF_LOG_FLUSH();
 
 	if(ret < 0)
 		return ret;
 	else
-		*data = rx_buf[len-1];
-
-	// send the SPI message (all of the above fields, inc. buffers)
+		*data = rx_buf[len];
+		// send the SPI message (all of the above fields, inc. buffers)
 	return ret;
 }
 
@@ -299,7 +309,7 @@ int cc1200_init(void)
 
 	APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
     NRF_LOG_INFO("SPI example\r\n");
-
+    //NRF_SPIM0->RXD.MAXCNT = 1;
     nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
     spi_config.ss_pin   = SPI_SS_PIN;
     spi_config.miso_pin = SPI_MISO_PIN;
@@ -347,9 +357,18 @@ int cc1200_init(void)
 
 	// Get Chip Info
 	cc1200_read_register(CC1200_PARTNUMBER, &partnum);
+	//NRF_SPIM0->ENABLE = 0;
+
+   	//nrf_drv_spi_uninit(&spi);
+	
+	//nrf_delay_ms(2000);
+	//NRF_SPI0->ENABLE = 1;
+	//APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler, NULL));
+	
 	cc1200_read_register(CC1200_PARTVERSION, &partver);
 	NRF_LOG_INFO("CC1200 Chip Number: 0x%x Chip Version: 0x%x\r\n", partnum, partver);
 	NRF_LOG_FLUSH();
+	
 
 	return 0;
 }
